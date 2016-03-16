43426e26d97ca4abb18e6f2931a0ece402b70381
ronathan <ronaldvictorino@gmail.com>
2016-03-16 11:11:10 -0400

    Added save and cancel buttons. Also learned about navigation controllers.

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -7,30 +7,54 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RatingControl!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    var meal: Meal?
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         // Handle the text field's user input through delegate callbacks.
         nameTextField.delegate = self
+        
+        checkValidMealName()
     }
     
     // MARK: UITextFieldDelegate
+    
     func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
         // Hide the keyboard
         textField.resignFirstResponder()
         return true
     }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+        saveButton.enabled = false
+    }
+    
     func textFieldDidEndEditing(textField: UITextField) {
-
+        
+        checkValidMealName()
+        navigationItem.title = textField.text
+    }
+    
+    func checkValidMealName() {
+        
+        let text = nameTextField.text ?? ""
+        saveButton.enabled = !text.isEmpty
     }
 
     // MARK: UIImagePickerControllerDelegate
+    
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
         // The info dictionary contains multiple representations of the image, and this uses the original.
         let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         
@@ -38,6 +62,21 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
         photoImageView.image = selectedImage
         
         // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: Navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if saveButton === sender {
+            let name = nameTextField.text ?? ""
+            let photo = photoImageView.image
+            let rating = ratingControl.rating
+            
+            meal = Meal(name: name, photo: photo, rating: rating)
+        }
+    }
+    @IBAction func cancel(sender: UIBarButtonItem) {
+        
         dismissViewControllerAnimated(true, completion: nil)
     }
     
